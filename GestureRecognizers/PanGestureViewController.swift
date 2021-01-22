b0ff73b2274cda01fe2e33c7840b752bbbfbff49
Darya Kuliashova <kuldiand@gmail.com>
2021-01-21 18:02:34 -0800

    list of gestures created

---
+++ GestureRecognizers/PanGestureViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PanGestureViewController.swift
+//  GestureRecognizers
+//
+//  Created by Darya Kuliashova on 21.01.21.
+//
+
+import UIKit
+
+class PanGestureViewController: UIViewController {
+
+}
